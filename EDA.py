dd61ac64f16f9254051cc2e3462bdb5efa0834c0
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 22:47:07 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,8 +4,9 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
-url = 'https://drive.google.com/file/d/19U3J_1CalIwvIccqtmNUfnfGfqEQ8EMj/view?usp=drive_link'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
+url='https://drive.google.com/file/d/1ZIxWQ_u5_PkHvPFLMOJjgPRm4IWSB4v_/view?usp=drive_link'
+path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df_news=pd.read_csv(path)
+df_news=df_news.dropna(axis=0)
 
 st.write(df)
