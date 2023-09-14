f2602f94fd8397568a298e9767648973ff49ccb5
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 22:58:35 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,12 +4,6 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-
-url='https://drive.google.com/file/d/1ZIxWQ_u5_PkHvPFLMOJjgPRm4IWSB4v_/view?usp=drive_link'
-path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df_news=pd.read_csv(path)
-df_news=df_news.dropna(axis=0)
+df=pd.read_csv("Dubawa_label_data.csv")
 
 st.write(df)
