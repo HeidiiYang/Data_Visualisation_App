86bd87748c4b41ae3b2f4d5cede43640b060de3c
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 22:24:45 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,10 +4,6 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
-url='https://drive.google.com/file/d/19U3J_1CalIwvIccqtmNUfnfGfqEQ8EMj/view?usp=drive_link'
-path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df=pd.read_csv(path)
 
-st.write(df)
   
 
