1566986c670b0b03598443409dcf6421b254d10d
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 21:47:53 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
-st.write(pd.DataFrame({'name': ['Luoie', 'Matt', 'Peter', 'Luck'], 'scores': [40, 55, 67, 73]}))
+uploaded_file=st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(dataframe)
+  
+
