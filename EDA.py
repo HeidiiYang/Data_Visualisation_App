f0f46b0131ad29a04205e082c64d096e656ac6c6
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 22:53:49 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,6 +4,9 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+
 url='https://drive.google.com/file/d/1ZIxWQ_u5_PkHvPFLMOJjgPRm4IWSB4v_/view?usp=drive_link'
 path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df_news=pd.read_csv(path)
