0939bd6fa5db60084adbabcc6a21e39b0144e1d5
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-14 21:59:07 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("My App")
 st.write("My first app")
 
-data=pd.read_csv("F:\Career\A-internship-remote projects\Fake news\data\updated_full_dataset.csv") 
+data=pd.read_csv("F:/Career/A-internship-remote projects/Fake news/data/updated_full_dataset.csv") 
 st.write(data)
   
 
