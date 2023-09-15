0f2e3ff580deee391cc42a7a0f94b6889e57cc90
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-15 12:02:40 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -6,10 +6,13 @@ streamlit config show
 
 st.title("My App")
 st.write("My first app")
-
+url = 'https://drive.google.com/file/d/1_cjfajizKAco3t8Sn99xODjcxcbYMshk/view?usp=drive_link'
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df = pd.read_csv(path)
 
 p=Path.cwd()
 st.write(p)
+st.write(df)
 
 
 
