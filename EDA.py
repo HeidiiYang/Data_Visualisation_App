f72a1dcf0fb75e8c4fb73c8401fb35691fd23806
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-15 13:51:23 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,12 +1,36 @@
 import streamlit as st
 import pandas as pd
-from pathlib import Path 
 
-st.title("My App")
-st.write("My first app")
-url = 'https://drive.google.com/file/d/1_cjfajizKAco3t8Sn99xODjcxcbYMshk/view?usp=drive_link'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
+# Code to read csv file into Colaboratory:
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+# Authenticate and create the PyDrive client.
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+drive_link=input("Enter link to the .csv: ")
+
+# This link can be replaced with any other Google Drive link to a .csv
+link=str(drive_link) # The shareable link
+
+fluff, id = link.split('=')
+print ("Link ID verification: ", id) # Verify that you have everything after '='
+
+filename_input = input("Enter desired filename (.csv will automatically be appended): ")
+
+filename = str(filename_input) + ".csv"
+
+downloaded = drive.CreateFile({'id':id}) 
+downloaded.GetContentFile(filename)  
+dataframe = pd.read_csv(filename)
+# Dataset is now stored in a Pandas Dataframe
+
+st.write("Data Import Complete.")
 
 p=Path.cwd()
 st.write(p)
