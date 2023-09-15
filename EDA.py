f9bbaaaa6ec19feadb2ed60dddad94721b789723
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-15 11:16:17 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,12 +2,11 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-
 st.title("My App")
 st.write("My first app")
 
 conn=st.experimental_connection("gsheets", type=GSheetsConnection)
 data=conn.read(worksheet="Dubawa_label_data")
-st.dataframe(data)
+df=st.dataframe(data)
 
 st.write(df)
