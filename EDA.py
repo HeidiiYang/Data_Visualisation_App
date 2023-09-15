efe8d7dba13825884b9c8c472c26abc0d5a1cff2
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-15 12:03:03 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path 
 
-streamlit config show
-
 st.title("My App")
 st.write("My first app")
 url = 'https://drive.google.com/file/d/1_cjfajizKAco3t8Sn99xODjcxcbYMshk/view?usp=drive_link'
@@ -12,6 +10,7 @@ df = pd.read_csv(path)
 
 p=Path.cwd()
 st.write(p)
+
 st.write(df)
 
 
