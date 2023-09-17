353ca55659c0657b145945c6d3ce84552615f0a3
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-17 17:34:04 +0100

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-df=pd.read_csv("data/Dubawa_label_data.csv")
+df_pos_tag=pd.read_csv("data/pos_tag.csv")
 
-st.write(df)
+#Visualisation
+fig=px.histogram(df_pos_tag, x='pos_tag', y='frequency_ratio', title='POS Tagging Frequency in Fake and Real News', barmode='group', color='news_category', color_discrete_sequence=px.colors.qualitative.Vivid)
+fig.update_xaxes(title='POS Tagging').update_yaxes(title='Frequency')
+fig.update_layout(xaxis={'categoryorder':'total descending'}, width=1400, height=500)
+fig.show()
+
+st.write(fig)
 st.write("Hello World")
 
 
