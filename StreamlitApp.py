430b9c29964b52a5cdd1cadde8dff54ca7912219
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-22 12:48:26 +0100

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 #Visualise the news count by year
 df_news_calculation=pd.read_csv("data/full_dataset_calculation.csv")
-fig1=px.histogram(df_news_calculation, x='year', title='News Count by Year', color='Label', color_discrete_sequence=px.colors.qualitative.Vivid)
+'''fig1=px.histogram(df_news_calculation, x='year', title='News Count by Year', color='Label', color_discrete_sequence=px.colors.qualitative.Vivid)
 fig1.update_xaxes(title='Publication Year').update_yaxes(title='News Count')
 fig1.update_layout(width=700, height=400, bargap=0.03)
 fig1.show()
@@ -13,10 +13,10 @@ df_pos_tag=pd.read_csv("data/pos_tag.csv")
 fig=px.histogram(df_pos_tag, x='pos_tag', y='frequency_ratio', title='POS Tagging Frequency in Fake and Real News', barmode='group', color='news_category', color_discrete_sequence=px.colors.qualitative.Vivid)
 fig.update_xaxes(title='POS Tagging').update_yaxes(title='Frequency')
 fig.update_layout(xaxis={'categoryorder':'total descending'}, width=1400, height=500)
-fig.show()
+fig.show()'''
 
 st.write(df_news_calculation)
-st.write(fig)
+
 
 
 
