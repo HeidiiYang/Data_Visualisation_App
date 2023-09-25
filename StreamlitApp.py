4780daf16d7880d8330e8ceedc0b53840b802355
Huidi Yang <93721870+HeidiiYang@users.noreply.github.com>
2023-09-25 21:40:15 +0100

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -101,7 +101,7 @@ fig3.update_layout(width=700, height=300)
 fig3.show()
 
 #Visualise the number of weords
-fig4=px.histogram(df_news_calculation, x='word_count', title='The number of words', barmode='overlay', color='Label', color_discrete_sequence=["#949CDF", "#EB455F"])
+fig4=px.histogram(df_news_calculation, x='word_count', title='Word Count', barmode='overlay', color='Label', color_discrete_sequence=["#949CDF", "#EB455F"])
 fig4.update_xaxes(title='The number of words').update_yaxes(title='News Count')
 fig4.update_layout(width=700, height=300)
 fig4.show()
@@ -113,7 +113,7 @@ fig5.update_layout(width=700, height=300)
 fig5.show()
 
 df_pos_tag=pd.read_csv("data/pos_tag.csv")
-fig6=px.histogram(df_pos_tag[df_pos_tag["frequency_ratio"]>1.3], x='pos_tag', y='frequency_ratio', title='POS Tagging Frequency in Fake and Real News', barmode='group', color='news_category', color_discrete_sequence=["#949CDF", "#EB455F"])
+fig6=px.histogram(df_pos_tag[df_pos_tag["frequency_ratio"]>1.3], x='pos_tag', y='frequency_ratio', title='Part-of-Speech Tagging Frequency in Fake and Real News', barmode='group', color='news_category', color_discrete_sequence=["#949CDF", "#EB455F"])
 fig6.update_xaxes(title='POS Tagging').update_yaxes(title='Frequency')
 fig6.update_layout(xaxis={'categoryorder':'total descending'}, width=700, height=400)
 fig6.show()
